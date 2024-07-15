771513ecbd1fb10c3d46e89acd9400f7e5e228b9
Alperen Yılmaz <yilmazmuha17@itu.edu.tr>
2024-07-15 03:22:23 +0300

    Move threadpool into library from vendor

--- build.bat
+++ build.bat
@@ -3,8 +3,8 @@ SETLOCAL
 
 set COMPILER_FLAGS=
 
-set INCLUDE=-Ivendor/raylib/include -Ivendor/threadpool/include
-set LIB=-Lvendor/raylib/lib -Lvendor/threadpool/lib
+set INCLUDE=-Ivendor/raylib/include -Ilibrary/threadpool/include
+set LIB=-Lvendor/raylib/lib -Llibrary/threadpool/lib
 
 set LINK=^
     -lraylib -lgdi32 -lwinmm ^
